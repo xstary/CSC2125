240550895ff5f1b111e933fd159df2166303162f
xing <xing.hu.utoronto@gmail.com>
2019-11-17 14:19:18 -0500

    random node-connecting method

---
+++ Simulator3.0/main.py
@@ -0,0 +1,95 @@
+import time
+from datetime import datetime
+from json import dumps as dump_json
+from world import SimulationWorld
+from nodeGenerator import NodeGenerator
+from network import Network
+from block import *
+
+
+def write_report(world):
+    with open('output/report.json', 'w') as f:
+        f.write(dump_json(world.env.data,indent=4))
+
+
+def report_node_chain(world, nodes_list):
+    for node in nodes_list:
+        head = node.chain.head
+        main_chain_list = node.chain.get_main_chain()
+        print(head.height)
+        
+        key = f'{node.nid}_chain'
+        world.env.data[key] = {
+            'nid': node.nid,
+            'region': node.region,
+            'hashrate': node.hashrate,
+            'head_block_hash': f'{head.hash} #{head.height}',
+            'number_of_total_known_blocks': len(node.known_blocks),
+            'number_of_total_blocks_inchain': node.chain.block_counter,
+            'number_of_blocks_on_main_chain': len(main_chain_list),
+            'main_chain_list': main_chain_list
+        }
+def report_system_summary(world, nodes_list): 
+    world.env.data['system summary']= {
+                'start_simulation_time': datetime.utcfromtimestamp(
+                    world._initial_time).strftime('%m-%d %H:%M:%S'),
+                'end_simulation_time': datetime.utcfromtimestamp(
+                    world._initial_time + world._sim_duration).strftime('%m-%d %H:%M:%S'),
+                'number_of_nodes': len(nodes_list),
+                'number_of_blocks': Block.nextBlockHash
+            }
+
+
+
+def run_model():
+    now = 0  # Current time
+    duration = 3600  # 1h in seconds
+
+    world = SimulationWorld(
+        duration,
+        now,
+        'configs/config.json',
+        'configs/latency.json',
+        'configs/throughput-rec.json',
+        'configs/throughput-send.json',
+        'configs/delays.json')
+
+    # Create the network
+    network = Network(world.env, 'Network0.0')
+
+    miners = {
+        'Ohio': {
+            'how_many': 1,
+            'mega_hashrate_range': (20, 40)
+        },
+        'Tokyo': {
+            'how_many': 2,
+            'mega_hashrate_range': (30, 50)
+        },
+        'Ireland': {
+            'how_many': 1,
+            'mega_hashrate_range': (0, 20)
+        }
+    }
+
+    node_generator = NodeGenerator(world, network)
+    # Create all nodes
+    nodes_list = node_generator.create_nodes(miners)
+    # Start the network heartbeat
+    world.env.process(network.start_heartbeat())
+    # Set the block generation prbability according to 
+    # total hashrate in the network and connect nodes
+    for node in nodes_list:
+        node.find_block_generation_probability()
+        node.connect(nodes_list)
+
+    world.start_simulation()
+
+    report_node_chain(world, nodes_list)
+    report_system_summary(world, nodes_list)
+    write_report(world)
+
+
+if __name__ == '__main__':
+    run_model()
+
